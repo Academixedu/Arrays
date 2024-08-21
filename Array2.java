001fb4a9192168ce874a78be7e18362e56d45d0a
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-08-21 17:57:55 +0530

    Revathi

--- Array2.java
+++ Array2.java
@@ -1,3 +1,10 @@
 public class Array2{
-  // Prepare an Array with values of 1,2,3,4,5 and replace the value at 3rd index with 45
+  public static void main(String[] args) {
+    // Prepare an Array with values of 1,2,3,4,5 and replace the value at 3rd index with 45
+    int[]a={1,2,3,4,5};
+    a[3]=45;
+    for(int p:a){
+      System.out.println(p);
+    }
+  }
 }
