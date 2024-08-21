c901dfdc900bf1d74ef853c1e163db6f2fd2f47f
kurojuLucky <luckykuroju28@gmail.com>
2024-08-21 19:29:51 +0530

    assign

--- Array3.java
+++ Array3.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 public class Array3{
     public static void main(String[] args) {
         Scanner kl=new Scanner(System.in);
-        int l[]=new int[5];
-        for(int i=0;i<=l.length-1;i++){
-            System.out.println("enter the number at :"+ i+"index");
-            int ok = kl.nextInt();
-            ok=l[i];  
-}
+        int nl[]=new int[5];
+        for(int i=0;i<=nl.length-1;i++){
+            System.out.println("enter the value"+ i+"index");
+            int ok=kl.nextInt();
+            nl[i]=ok;
+        }
+        System.out.println("using for each loop");
+        for (int p : nl) {
+            System.out.println(p);
+        }
     }
 }
