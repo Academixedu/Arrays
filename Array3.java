94beb69776e6210865aaa7192c45148f4761eb38
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-08-21 17:47:16 +0530

    shahid

--- Array3.java
+++ Array3.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class Array3{
 // Prepare an Element of an Array and Implement Values using Scanner
+public static void main(String[] args) {
+    Scanner b=new Scanner(System.in);
+    int[] f=new int[5];
+    for(int i=0;i<=f.length-1;i++){
+        System.out.println("enter"+i+"value");
+        int z=b.nextInt();
+        f[i]=z;
+
+    }
+    for (int i : f) {
+        System.out.println(i);
+    }
+    
+    }
 }
+
--- Arrays1.java
+++ Arrays1.java
@@ -1,3 +1,9 @@
 public class Arrays1{
 // Create an Array and Initialize it Iterate  it by using For Loop in Reverse
+public static void main(String[] args){
+    int[] a={1,2,3,4,5};
+    for(int i=4;i<=a.length-1;i--){
+        System.out.println(a[i]);
+    }
+}
 }
