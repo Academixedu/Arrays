d70dc471ee4fad2af74b906b76c11ce3d283e771
Talari-Lavanya <lavanya62811@gmail.com>
2024-08-21 17:53:26 +0530

    Lavanya

--- Array2.java
+++ Array2.java
@@ -1,3 +1,12 @@
 public class Array2{
   // Prepare an Array with values of 1,2,3,4,5 and replace the value at 3rd index with 45
+  public static void main(String[] args){
+    int[]arr={1,2,3,4,5};
+    arr[3]=45;
+    for(int i=0;i<=arr.length-1;i++){
+
+      System.out.println(arr[i]);
+
+    }
+  }
 }
--- Arrays1.java
+++ Arrays1.java
@@ -1,3 +1,13 @@
 public class Arrays1{
 // Create an Array and Initialize it Iterate  it by using For Loop in Reverse
+public static void main(String[] args){
+    int[]arr={1,2,3,4,5};
+    for(int  i= arr.length - 1; i >= 0; i--){
+        
+        System.out.println("reverse of a number");
+        System.out.println(arr[i]);
+    }
+
+
+}
 }
