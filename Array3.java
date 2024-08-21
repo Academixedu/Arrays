e1b7f2e1ccae17180600f2bfde4a7640316d7765
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-08-21 17:48:11 +0530

    Shahid

--- Array3.java
+++ Array3.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
     Scanner b=new Scanner(System.in);
     int[] f=new int[5];
     for(int i=0;i<=f.length-1;i++){
-        System.out.println("enter"+i+"value");
+        System.out.println("enter"+i+"index");
         int z=b.nextInt();
         f[i]=z;
 
