dc7e3f67a00db862c4364adcaec9f4fabf4745a4
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-08-21 17:23:32 +0530

    Update Arrays1.java

--- Arrays1.java
+++ Arrays1.java
@@ -4,6 +4,7 @@ public class Arrays1{
      public static void main(String[] args) {
         int[] p = new int[5];
         Scanner sc = new Scanner(System.in);
+          System.out.println("enter the values for reversing");
         for (int i = 0; i < p.length; i++) {
             p[i] = sc.nextInt();
         }
