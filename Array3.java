28e47e74d3cf8844aeca64dbfb71d6e9f4a85cda
karthhiik <karthikthatikonda18@gmail.com>
2024-08-21 20:21:34 +0530

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class Array3{
 // Prepare an Element of an Array and Implement Values using Scanner
+  public static void main(String[] args) {
+        int[] p = new int[5];
+        Scanner sc = new Scanner(System.in);
+     
+        for (int i = 0; i < p.length; i++) {
+          System.out.println("enter the values of index"+i);
+            p[i] = sc.nextInt();
+        }
+        System.out.println("Printing the reverse numbers");
+        for (int i = 0;i<p.length; i++) {
+            System.out.println(p[i]);
+        }
+    }
 }
+  
+
