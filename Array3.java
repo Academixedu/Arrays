b9baa1e360cf40a8a6c0b31085b18fd2d515ec82
kurojuLucky <luckykuroju28@gmail.com>
2024-08-21 19:18:18 +0530

    assi

--- Array3.java
+++ Array3.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
 public class Array3{
-// Prepare an Element of an Array and Implement Values using Scanner
+    public static void main(String[] args) {
+        Scanner kl=new Scanner(System.in);
+        int l[]=new int[5];
+        for(int i=0;i<=l.length-1;i++){
+            System.out.println("enter the number at :"+ i+"index");
+            int ok = kl.nextInt();
+            ok=l[i];  
+}
+    }
 }
