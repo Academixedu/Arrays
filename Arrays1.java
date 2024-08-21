598a42965b9d12f9fb22ea2dd9b7237cb2b4d26a
Arbaz-01 <arbazshaibu@gmail.com>
2024-08-21 18:05:03 +0530

    arbazzzzz

--- Arrays1.java
+++ Arrays1.java
@@ -1,3 +1,11 @@
 public class Arrays1{
+    public static void main(String[] args) {
+        int []z= {1,2,3,4,5,6,7};
+        System.out.println(" Loop in Reverse ");
+        for(int i=z.length-1;i>=0;i--){
+            System.out.println(z[i]);
+        }
+     
+    }
 // Create an Array and Initialize it Iterate  it by using For Loop in Reverse
 }
