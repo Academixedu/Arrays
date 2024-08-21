6364a4d3e084b39dd76e3291ab440b0dfdb9f61b
vijay767492 <vijaydandu59@gmail.com>
2024-08-21 19:07:56 +0530

    vijay dandu/arrays

--- Arrays1.java
+++ Arrays1.java
@@ -1,3 +1,10 @@
 public class Arrays1{
 // Create an Array and Initialize it Iterate  it by using For Loop in Reverse
+public static void main(String[] args) {
+    int[] marks={95,98,99,56,65,78,90,34,22,12,65,89,90};
+    for(int i=marks.length-1;i>=0;i--){
+        System.out.println("printing the marks in reverse order :"+marks[i]);
+    }
+    
+}
 }
