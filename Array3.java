3016a28672ca8c086e2e848865f2f9d9d8a0b731
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-21 18:09:00 +0530

    done

--- Array3.java
+++ Array3.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
+
 public class Array3{
 // Prepare an Element of an Array and Implement Values using Scanner
+public static void main(String[] args) {
+    
+    Scanner on=new Scanner(System.in);
+        int[] out=new int[4];
+        for(int i=0;i<=out.length-1;i++)
+        {
+            System.out.println("enter any no"+i+"index");
+            int in=on.nextInt();    
+
+            out[i]=in;
+
+    }
+    for(int h:out)
+    {
+        System.out.println(h);
+    }
+}
 }
